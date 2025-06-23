177e2354266a48d40645fbd003acd0d984364169
feliipecardosoo <fglibino@gmail.com>
2025-06-23 20:56:39 -0300

    adicionando mudancas

--- src/model/service/createMembro.go
+++ src/model/service/createMembro.go
@@ -4,6 +4,7 @@ import (
 	"crud/src/configuration/logger"
 	resterr "crud/src/configuration/rest-err"
 	"crud/src/model"
+	"fmt"
 
 	"go.uber.org/zap"
 )
@@ -11,5 +12,7 @@ import (
 func (ud *membroDomainService) CreateMembroModel(userDomain model.MembroDomainInterface) *resterr.RestErr {
 	logger.Info("Init createMembro model", zap.String("jorney", "createMembro"))
 	userDomain.EncryptPassword()
+	fmt.Println(userDomain.GetName())
+	fmt.Println(userDomain.GetPassword())
 	return nil
 }
